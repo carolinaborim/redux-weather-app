204a22455c3b77b12b1ac8bf60094500e6946a2b
Carolina Borim <carolina.borim@gmail.com>
2017-02-20 23:17:09 -0500

    Brings data for the WeatherList from the App State

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 export default class WeatherList extends Component {
   render(){
@@ -7,6 +8,9 @@ export default class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
@@ -14,4 +18,10 @@ export default class WeatherList extends Component {
       </table>
     );
   }
+}
+
+function mapStateToProps(state){
+  return {
+    weather: state.weather
+  };
 }
