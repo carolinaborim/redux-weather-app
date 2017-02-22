3322dc2c203a41c0b1ab3e39c8983cf5259f0ad4
Carolina Borim <carolina.borim@gmail.com>
2017-02-21 19:06:30 -0500

    Fetches temperatures and Adds Sparkline Chart for the app

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
+    const temperatures = cityData.list.map(weather => weather.main.temp);
 
     return(
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} witdth={180} data={temperatures}>
+            <SparklinesLine color="purple"/>
+          </Sparklines>
+        </td>
       </tr>
     );
   }
